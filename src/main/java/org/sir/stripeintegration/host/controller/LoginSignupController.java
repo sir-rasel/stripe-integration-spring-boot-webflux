dfd077812c0ba732637bd9b46bea4d194e976871
Saiful Islam Rasel <raselcse97@gmail.com>
2023-07-02 21:27:47 +0600

    add word api to the login signup endpoint

--- src/main/java/org/sir/stripeintegration/host/controller/LoginSignupController.java
+++ src/main/java/org/sir/stripeintegration/host/controller/LoginSignupController.java
@@ -8,12 +8,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 @AllArgsConstructor
 @RestController
 @Validated
+@RequestMapping("/api")
 public class LoginSignupController {
     private IUserService userService;
 
